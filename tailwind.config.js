11f489c21f0ffe34cd5dae87b978ff81f2d9dced
tarunAllmysons <tarun@in.allmysons.com>
2025-05-25 18:18:32 +0530

    tailwind 3 practise

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        chestnut: "#973F29",
+      },
+    },
+  },
+  plugins: [],
+};
